64e5dae59bd8dad1d05c2fbce434a300da979854
Michael Rockingham <mrockingham2@gmail.com>
2020-07-09 13:19:55 -0400

    update package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
